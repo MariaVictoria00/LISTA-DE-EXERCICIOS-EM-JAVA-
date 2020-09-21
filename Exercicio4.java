0d8e987447ad61a804bf568bd39dd64c670e6c9b
Maria Victória de Moraes Souza <65103555+MariaVictoria00@users.noreply.github.com>
2020-09-20 23:08:39 -0400

    Add files via upload

---
+++ Exercicio4.java
@@ -0,0 +1,27 @@
+package ExerciciosDoModulo;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+	 public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("DIGITE UM NUMERO");
+		double numero=entrada.nextDouble();
+		int contador=0;
+		for(int i=2;i<numero;i++) {
+     		if(numero%i==0) {
+     			contador++;
+		    }
+		}
+		if(numero!=1 &&contador==0) {
+			System.out.println("è um numero primo");
+		}
+		else {
+			System.out.println("Não é um numero primo");
+		}
+		
+		
+		entrada.close();
+	}
+
+}
